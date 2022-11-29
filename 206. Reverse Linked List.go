22781d8242771f33f7b6d5e82ad67ec21dbcb724
Akhilan <akhilkrishnans@gmail.com>
2022-11-29 11:01:44 +0530

    206. Reverse Linked List

---
+++ 206. Reverse Linked List.go
@@ -0,0 +1,27 @@
+// https://leetcode.com/problems/reverse-linked-list/
+
+
+//Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+    var prev *ListNode
+    
+    
+    for head != nil {
+        temp := head.Next
+		head.Next = prev
+		prev = head
+		head = temp
+        
+    }
+    
+    return prev
+}
