d58564a5964a7d60b3d8bf2ee138483db0f31076
Akhilan <akhilkrishnans@gmail.com>
2022-12-06 12:52:26 +0530

    1965. Employees With Missing Information

---
+++ 1965. Employees With Missing Information.sql
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/employees-with-missing-information/description/
+
+SELECT employee_id 
+FROM employees
+WHERE employee_id NOT IN (SELECT employee_id FROM salaries)
+UNION
+SELECT employee_id 
+FROM salaries
+WHERE employee_id NOT IN (SELECT employee_id FROM employees)
+ORDER BY employee_id;
