4e1ddec1d32d49d9ba2add204e1bfee80fd21db6
Akhilan <akhilkrishnans@gmail.com>
2022-12-09 13:46:15 +0530

    938. Range Sum of BST

---
+++ 938. Range Sum of BST.go
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/range-sum-of-bst/description/
+
+// Given the root node of a binary search tree and two integers low and high, return the sum of values of all nodes with a value in the inclusive range [low, high].
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func rangeSumBST(root *TreeNode, low int, high int) int {
+    
+    if root == nil {
+		return 0
+	}
+	if root.Val > high {
+		return rangeSumBST(root.Left, low, high)
+	}
+	if root.Val < low {
+		return rangeSumBST(root.Right, low, high)
+	}
+	return root.Val + rangeSumBST(root.Left, low, high) + rangeSumBST(root.Right, low, high)
+}
+
