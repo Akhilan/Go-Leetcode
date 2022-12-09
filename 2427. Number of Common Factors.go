460f13433e786d72d970f445c465b0e97a65a079
Akhilan <akhilkrishnans@gmail.com>
2022-12-09 13:40:32 +0530

    2427. Number of Common Factors

---
+++ 2427. Number of Common Factors.go
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/number-of-common-factors/description/
+
+func commonFactors(a int, b int) int {
+    ans := 0
+    for i := 1; i <= a || i <= b; i++ {
+        if a % i == 0 && b % i == 0 {
+            ans++
+        }
+    }
+    return ans
+}
+
