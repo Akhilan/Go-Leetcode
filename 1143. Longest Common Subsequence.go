91b3ba54195e2b203d4aad099f168443b9b4be80
Akhilan <akhilkrishnans@gmail.com>
2022-12-15 13:36:13 +0530

    1143. Longest Common Subsequence.go

---
+++ 1143. Longest Common Subsequence.go
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/longest-common-subsequence/description/
+
+func longestCommonSubsequence(text1 string, text2 string) int {
+    l1 := len(text1)
+    l2 := len(text2)
+    dp := make([][]int, l1 + 1)
+    for i := range dp {
+        dp[i] = make([]int, l2 + 1)
+    }
+    for i := 1; i <= l1; i++ {
+        for j := 1; j <= l2; j++ {
+            if text1[i - 1] == text2[j - 1] {
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+            }
+        }
+    }
+    return dp[l1][l2]
+}
+
+func max(a, b int) int {
+    if a > b { return a }
+    return b
+}
